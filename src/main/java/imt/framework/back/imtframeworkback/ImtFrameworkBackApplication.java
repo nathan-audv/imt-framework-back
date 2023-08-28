555da60946d89878b36180f58c3a254045f0f858
naudvard <nathan.audvard@adeo.com>
2023-08-28 13:28:04 +0200

    setup

---
+++ src/main/java/imt/framework/back/imtframeworkback/ImtFrameworkBackApplication.java
@@ -0,0 +1,13 @@
+package imt.framework.back.imtframeworkback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImtFrameworkBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImtFrameworkBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/imt/framework/back/imtframeworkback/ImtFrameworkBackApplicationTests.java
@@ -0,0 +1,13 @@
+package imt.framework.back.imtframeworkback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImtFrameworkBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
