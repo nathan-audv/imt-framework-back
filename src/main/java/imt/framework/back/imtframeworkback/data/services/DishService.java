4799137bb4a80397a4ad7c7d06d2d52c1fd65961
Brice Redon <briceredon35@yahoo.fr>
2023-08-31 10:57:15 +0200

    GET all dishes

--- src/main/java/imt/framework/back/imtframeworkback/data/services/DishService.java
+++ src/main/java/imt/framework/back/imtframeworkback/data/services/DishService.java
@@ -4,6 +4,8 @@ import imt.framework.back.imtframeworkback.data.models.DishModel;
 import imt.framework.back.imtframeworkback.domain.models.Dish;
 import java.util.List;
 
+import java.util.List;
+
 public interface DishService {
     public void save(Dish dish);
     public List<Dish> findAll();
