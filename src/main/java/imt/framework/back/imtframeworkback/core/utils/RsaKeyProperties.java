71925b0b3b5228ce4280fc25c5938722276eb1da
naudvard <nathan.audvard@adeo.com>
2023-10-20 13:04:20 +0200

    RSA keys

--- src/main/java/imt/framework/back/imtframeworkback/core/utils/RsaKeyProperties.java
+++ src/main/java/imt/framework/back/imtframeworkback/core/utils/RsaKeyProperties.java
@@ -1,10 +1,24 @@
 package imt.framework.back.imtframeworkback.core.utils;
 
+import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
-@ConfigurationProperties(prefix = "rsa")
-public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+@Component
+@Getter
+public class RsaKeyProperties {
+
+    private final RSAPublicKey publicKey;
+    private final RSAPrivateKey privateKey;
+
+    public RsaKeyProperties(){
+        KeyPair pair = KeyGenerator.generateRsaKey();
+        this.publicKey = (RSAPublicKey) pair.getPublic();
+        this.privateKey = (RSAPrivateKey) pair.getPrivate();
+    }
 }
